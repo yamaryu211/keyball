2bc405aef69928cec29bd3398cb237b4299eb912
yamaryu211 <yamaryu380@gmail.com>
2024-12-26 09:37:17 +0800

    mod: レイヤー・モディファイアキーを含むコンボキーの設定を追加

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/keymap.c
@@ -111,6 +111,10 @@ void oledkit_render_info_user(void) {
   const uint16_t PROGMEM combo_left_click[] = {KC_J, KC_K, COMBO_END};
   const uint16_t PROGMEM combo_right_click[] = {KC_K, KC_L, COMBO_END};
   const uint16_t PROGMEM combo_middle_click[] = {KC_J, KC_L, COMBO_END};
+  // レイヤー・モディファイアキーを含むコンボキーの設定
+  const uint16_t PROGMEM combo_left_click2[] = {LT(1, KC_J), LT(2, KC_K), COMBO_END};
+  const uint16_t PROGMEM combo_right_click2[] = {LT(2, KC_K), MT(MOD_LALT, KC_L), COMBO_END};
+  const uint16_t PROGMEM combo_middle_click2[] = {LT(1, KC_J), MT(MOD_LALT, KC_L), COMBO_END};
 
   combo_t key_combos[] = {
       [0] = COMBO(combo_esc, KC_ESC),
@@ -118,5 +122,8 @@ void oledkit_render_info_user(void) {
       [2] = COMBO(combo_left_click, KC_BTN1),
       [3] = COMBO(combo_right_click, KC_BTN2),
       [4] = COMBO(combo_middle_click, KC_BTN3),
+      [5] = COMBO(combo_left_click2, KC_BTN1),
+      [6] = COMBO(combo_right_click2, KC_BTN2),
+      [7] = COMBO(combo_middle_click2, KC_BTN3),
   };
 #endif
