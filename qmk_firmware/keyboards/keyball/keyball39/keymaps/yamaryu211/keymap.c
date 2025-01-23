62d2eed3a23f9eed5a44c927a094a3e18933a1cc
yamaryu211 <yamaryu380@gmail.com>
2025-01-23 12:07:19 +0800

    mod: レイヤー4のみ消灯

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/keymap.c
@@ -81,18 +81,18 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = biton32(state);
     switch (layer)
     {
-    case 0:
-        rgblight_enable_noeeprom();
-        rgblight_sethsv(0, 0, 10);
-        break;
-    case 2:
-        rgblight_enable_noeeprom();
-        rgblight_sethsv(120, 100, 10);
-        break;
-    case 3:
-        rgblight_enable_noeeprom();
-        rgblight_sethsv(240, 100, 10);
-        break;
+    // case 0:
+    //     rgblight_enable_noeeprom();
+    //     rgblight_sethsv(0, 0, 10);
+    //     break;
+    // case 2:
+    //     rgblight_enable_noeeprom();
+    //     rgblight_sethsv(120, 100, 10);
+    //     break;
+    // case 3:
+    //     rgblight_enable_noeeprom();
+    //     rgblight_sethsv(240, 100, 10);
+    //     break;
     case 4:
         rgblight_disable_noeeprom();
         break;
